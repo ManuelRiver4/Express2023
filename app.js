59660ac007eba426a827d7fbdc89e51166bb31c6
Manuel Alberto Rivera Borrego <Manuel114xd@gmail.com>
2023-04-28 09:28:09 -0600

    Manejo de Rutas

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-res.send('Hola Mundo!!');
+res.send('Una API basica desde Express');
 
 });
+
+app.get('/saludo', function(req, res){
+    res.send('Hola desde la API');
+    
+    });
 
 app.listen(3000, function(){
-console.log('Aplicacion ejemplo, escuchando el puerto 3000!')
+console.log('Adios desde una API')
 
 });
+
