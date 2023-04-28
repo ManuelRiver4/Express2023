46c246fe62be0db9f36052b47dfc0936ef2afd8b
Manuel Alberto Rivera Borrego <Manuel114xd@gmail.com>
2023-04-28 09:54:12 -0600

    bodyParse

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-var express = require('express');
-var app = express();
+// require packages and set the port
+const express = require('express');
+const port = 3002;
 
-app.get('/', function(req, res){
-res.send('Una API basica desde Express');
+//para permitir manejo de POST y PUT
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes')
 
+// Usar node is body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+
+    extended: true,
+}));
+
+routes(app);
+
+// iniciar el servidor
+const server = app.listen(port, (error) => {
+    if (error) return console.log('Error: ${error}');
+
+    console.log('El servidor escucha en el puerto ${server.address().port}');
 });
 
-app.get('/saludo', function(req, res){
-    res.send('Hola desde la API');
-    
-    });
-
- app.get('/despedida', function(req, res){
- res.send('Adios desde una API');
-        
-        });
 
