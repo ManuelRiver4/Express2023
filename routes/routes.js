81a46969d43cabf148db2047863236d22b5fab8d
Manuel Alberto Rivera Borrego <Manuel114xd@gmail.com>
2023-05-08 09:27:45 -0600

    Agregar un nuevo usuario

--- routes/routes.js
+++ routes/routes.js
@@ -27,8 +27,20 @@ app.get('/users/:id', (request, response) => {
   pool.query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
     if (error) throw error;
     response.send(result);
-  })
-})
+  });
+});
+
+//Agregar un nuevo usuario
+app.post('/users', (request, response) => {
+  post.query('INSERT INTO users SET ?', request.body, (error, result) => {
+    if (error) throw error;
+
+    response.status(201).send('User added with ID: ${result.insertId}' );
+  });
+});
+
+
+
 
 
 };
